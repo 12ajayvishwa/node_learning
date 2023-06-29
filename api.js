12aa20c5cd5d553afe8acdc60ae841446970a4b0
12ajayvishwa <ajayvish21099@gmail.com>
2023-06-29 18:57:48 +0530

    second

--- api.js
+++ api.js
@@ -2,11 +2,19 @@ const express = require('express');
 const dbConnect = require('./mongodb');
 const app = express();
 
-app.get('/',async (req,resp) =>{
+app.use(express.json());
+
+app.get('/', async (req, resp) => {
     let data = await dbConnect();
     data = await data.find().toArray();
     console.log(data)
     resp.send(data)
 });
+
+app.post('/', async (req, resp) => {
+    let data = await dbConnect()
+    let result = data.insertOne(req.body)
+    resp.send(result)
+})
 
 app.listen(5000);
